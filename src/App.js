54920c15d5edd241db3aebc0b9d35b5d3c18973c
Phil DeSousa <80587045+phides-code@users.noreply.github.com>
2022-10-07 17:58:57 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState, createRef } from "react";
+import Bubble from "./Bubble";
+import AnimateBubbles from "./AnimateBubbles";
+import initialImages from "./initialImages";
+import shuffleArray from "./helpers/shuffleArray";
+import "./styles.css";
+
+export default function App() {
+  const [images, setImages] = useState(initialImages);
+
+  const reorder = () => {
+    const shuffledImages = shuffleArray(images);
+    setImages(shuffledImages);
+  };
+
+  return (
+    <div>
+      <div className="bubbles-wrapper">
+        <div className="bubbles-group">
+          <AnimateBubbles>
+            {images.map(({ id, text }) => (
+              <Bubble key={id} id={id} text={text} ref={createRef()} />
+            ))}
+          </AnimateBubbles>
+        </div>
+      </div>
+
+      <div className="button-wrapper">
+        <button className="button" onClick={reorder}>
+          Re-order images
+        </button>
+      </div>
+    </div>
+  );
+}
